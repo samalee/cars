1c225d8edf16655eaad6362e7c2f6d1a67c0e256
Samantha Malay <samaleemalay@gmail.com>
2019-09-10 19:49:42 -0600

    test sort of passes

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,38 +1,41 @@
 class Car < ApplicationRecord
   # validates_presence_of :make
-  # validates_uniqueness_of :make
+  # validates_uniqueness_of :make 
 
-  validates :make, uniqueness: true, presense: trure
+  validates :make, uniqueness: true, presence: true
 
-  #return all cars ascendding order by model
+  # return all cars asc order by model
   def self.by_model
-
+    order(:model)
   end
 
-  #return all cars ascendding order by make
+  # return all cars asc order by make
   def self.by_make
-
+    # Car.order(:make)
+    order(:make)
   end
 
-  #no params pass return ascending all the car by mileage
+  # no params pass return asc all the car by mileage
   # :desc order by mileage
   def self.by_mileage(direction = :asc)
-
+    # Car.order(mileage: :asc)
+    # Car.order(mileage: :desc)
+    order(mileage: direction)
   end
 
-  #chnage the color of the car with the param
+  # change the color of the car with the param
   def paint(color)
-
+    self.update(color: color)
   end
 
-  #return a hash of all the car's infor 
+  # return a hash of all the car's info 
   def info
-
+    self.attributes.except('id', 'created_at', 'updated_at')
   end
 
-  #return a string of the car horn
+  # return a string of the car horn
   def honk
-
+    'Loud!!!'
   end
 
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -28,4 +28,59 @@ RSpec.describe Car, type: :model do
     it { should validate_presence_of(:make) }
   end
 
+  describe 'class methods' do
+    before(:each) do
+      @car1 = Car.create(make: 'Tesla', model: 'TZ', mileage: 0)
+      @car2 = Car.create(make: 'Honda', model: 'Cooler', mileage: 1234)
+      @car3 = Car.create(make: 'Kia', model: 'Optima', mileage: 324214)
+    end
+
+    it 'should sort by make :asc' do
+      expect(Car.by_make).to eq([@car2, @car3, @car1])
+    end
+
+    it 'should sort by model :asc' do
+      expect(Car.by_model).to eq([@car2, @car3, @car1])
+    end
+
+    it 'should sort by mileage :asc' do
+      expect(Car.by_mileage).to eq([@car1, @car2, @car3])
+    end
+
+    it 'should sort by mileage :desc' do
+      expect(Car.by_mileage(:desc)).to eq([@car3, @car2, @car1])
+    end
+  end
+
+  describe 'instance methods' do
+    before(:each) do
+      @attr = {
+        make: 'Toyata',
+        model: 'Prius',
+        color: 'Purple',
+        mileage: 100000,
+        wheels: true,
+        year: 2012
+      }
+
+      @car = Car.create(@attr)
+    end
+
+    it 'should paint the car' do
+      @car.paint('Blue')
+      expect(@car.color).to eq('Blue')
+    end
+
+    it 'should return the car info' do
+      expect(@car.info).to eq(@attr.with_indifferent_access)
+    end
+
+    it 'should honk' do
+      expect(@car.honk).to eq('Loud!!!')
+    end
+  end
+
 end
+
+
+
